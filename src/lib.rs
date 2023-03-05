f16736cd52f71df93226ab22237a90033f689b71
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2023-03-05 13:19:04 +0000

    fix module name

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use crate::enums::schedule;
 use crate::enums::Weekday;
 
 #[pymodule]
-fn field_enums(_py: Python, m: &PyModule) -> PyResult<()> {
+fn rust_field_enums_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Weekday>()?;
     m.add_function(wrap_pyfunction!(schedule, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_day, m)?)?;
